a16dbdeaeba0d96b492390c7c1a18d9a441c5aa9
Ranul01 <ranulhome@gmail.com>
2024-09-20 13:49:46 +0530

    added add button

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,6 +4,9 @@ import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
 import Category from '../../components/Home/Category'
 import PetListByCategory from '../../components/Home/PetListByCategory'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Colors from '../../constants/Colors'
+import { TouchableOpacity } from 'react-native'
 
 export default function Home() {
     return (
@@ -21,6 +24,26 @@ export default function Home() {
             <PetListByCategory />
 
             {/*Add new pet option*/}
+            <TouchableOpacity style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 20,
+                gap: 20,
+                marginTop: 20,
+                backgroundColor: Colors.LIGHT_PRIMARY,
+                borderWidth: 1,
+                borderColor: Colors.PRIMARY,
+                borderRadius: 15,
+                borderStyle: 'dashed',
+            }}>
+                <MaterialIcons name="pets" size={24} color={Colors.PRIMARY} />
+                <Text style={{
+                    fontFamily: 'outfit',
+                    color: Colors.PRIMARY
+                }}>Add new pet</Text>
+            </TouchableOpacity>
         </View>
     )
 }
